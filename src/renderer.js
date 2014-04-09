e513a0ae997dafb54c7e4ce191941276f2f741e6
Taehoon Moon <panarch@kaist.ac.kr>
2014-04-09 22:22:03 +0900

    Added clear and update function to renderer.

--- src/renderer.js
+++ src/renderer.js
@@ -17,6 +17,7 @@ Concerto.Renderer = function($container, musicjson, options) {
     
     var numPages = Concerto.Parser.getNumPages(musicjson);
     var pages = [];
+    var doms = [];
     for(var i = 0; i < numPages; i++) {
         var divId = 'content' + i;
         var $div = $('<div>', {id: divId});
@@ -29,18 +30,55 @@ Concerto.Renderer = function($container, musicjson, options) {
         
         var vexflowRenderer = new Vex.Flow.Renderer(divId, backends);
         var ctx = vexflowRenderer.getContext();
-        pages.push(ctx); 
+        pages.push(ctx);
+        doms.push($div);
     }
 
     this.musicjson = musicjson;
     this.numPages = numPages;
     this.pages = pages;
+    this.doms = doms;
 };
 
 /**
  * @this {Concerto.Renderer}
  */
-Concerto.Renderer.prototype.draw = function() {
-    Concerto.Parser.parseAndDraw(this.pages, this.musicjson);
+Concerto.Renderer.prototype.draw = function(page) {
+    var pages;
+    if(page === undefined) {
+        pages = this.pages;
+    }
+    else {
+        for(var i = 0; i < pages.length; i++) {
+            if(page === i) {
+                pages.push(pages[i]);
+            }
+            else {
+                pages.push(undefined);
+            }
+        }
+    }
+
+    Concerto.Parser.parseAndDraw(pages, this.musicjson);
+};
+
+Concerto.Renderer.prototype.clear = function(page) {
+    var all = false;
+    if(page === undefined) {
+        all = true;
+    }
+
+    for(var i = 0; i < this.doms.length; i++) {
+        if(all || page === i) {
+            var $dom = this.doms[i];
+            $dom.find('svg').empty();
+        }
+    }
+};
+
+Concerto.Renderer.prototype.update = function(page) {
+    // redraw only specific page
+    this.clear(page);
+    this.draw(page);
 };
 
