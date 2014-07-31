55b03e5de3e770e589c12522e1aa893a61a6642e
Taehoon Moon <panarch@kaist.ac.kr>
2014-08-01 00:43:26 +0900

    Implemented supporting adding/deleting page when draw called in renderer

--- src/renderer.js
+++ src/renderer.js
@@ -10,40 +10,59 @@
  * @template Concerto.Renderer
  */
 Concerto.Renderer = function($container, musicjson, options) {
-    var backends = Vex.Flow.Renderer.Backends.RAPHAEL; 
+    this.backends = Vex.Flow.Renderer.Backends.RAPHAEL;
     if(options && options.backends) {
-        backends = options.backends;
+        this.backends = options.backends;
     }
-    
-    var numPages = Concerto.Parser.getNumPages(musicjson);
-    var pages = [];
-    var doms = [];
-    for(var i = 0; i < numPages; i++) {
-        var $div = $('<div>');
-        $div.addClass('content' + i);
-        
-        var size = Concerto.Parser.getPageSize(musicjson);
-        $div.css('width', size.width)
-            .css('height', size.height);
 
-        $container.append($div);
-        
-        var vexflowRenderer = new Vex.Flow.Renderer($div[0], backends);
-        var ctx = vexflowRenderer.getContext();
-        pages.push(ctx);
-        doms.push($div);
+    this.$container = $container;
+
+    this.numPages = Concerto.Parser.getNumPages(musicjson);
+    this.pages = [];
+    this.doms = [];
+    this.pageSize = Concerto.Parser.getPageSize(musicjson);
+    for(var i = 0; i < this.numPages; i++) {
+        this.addPage();
     }
 
     this.musicjson = musicjson;
-    this.numPages = numPages;
-    this.pages = pages;
-    this.doms = doms;
+};
+
+/**
+ * @this {Concerto.Renderer}
+ */
+Concerto.Renderer.prototype.addPage = function() {
+    var $div = $('<div>');
+
+    $div.css('width', this.pageSize.width)
+        .css('height', this.pageSize.height);
+
+    $div.addClass('concerto-page');
+
+    this.$container.append($div);
+
+    var vexflowRenderer = new Vex.Flow.Renderer($div[0], this.backends);
+    var ctx = vexflowRenderer.getContext();
+    this.pages.push(ctx);
+    this.doms.push($div);
 };
 
 /**
  * @this {Concerto.Renderer}
  */
 Concerto.Renderer.prototype.draw = function(page) {
+    var numPages = Concerto.Parser.getNumPages(this.musicjson);
+    if(numPages !== this.numPages) {
+        if(numPages > this.numPages) {
+            this.addPage();
+        }
+        else {
+            // remove last child
+            this.$container.find('.concerto-page:last-child').remove();
+        }
+        this.numPages = numPages;
+    }
+
     var pages;
     if(page === undefined) {
         pages = this.pages;
