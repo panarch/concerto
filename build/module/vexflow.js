cc68215e88bf90885f759feb9152d506c1d2deb7
Taehoon Moon <panarch@kaist.ac.kr>
2014-11-17 21:54:57 +0900

    Updated build

--- build/module/vexflow.js
+++ build/module/vexflow.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
 var $ = require("jquery");
+var Raphael = require("raphael");
 /**
  * VexFlow Engraver 1.2 Custom
  * A library for rendering musical notation and guitar tablature in HTML5.
