6d2d84b1754ff7d6142bbaa56149ca1f845f579a
Taehoon Moon <panarch@kaist.ac.kr>
2015-12-21 08:44:14 +0000

    Restart! reset all!

--- grunt/jscs.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  src: ['src/**/**.js', 'demo/**.js', 'Gruntfile.js'],
-  options: {
-    config: '.jscsrc'
-  }
-};
--- wraps/end.frag.js
+++
@@ -1,5 +0,0 @@
-    // wrap-end.frag.js
-
-    // change "my-lib" to your 'entry-point' module name 
-    return require('Concerto');
-}));
